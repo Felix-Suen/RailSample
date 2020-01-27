7832c51b4ed517ce386007e3c2c69df625d100ed
Felix <felix.suen2000@gmail.com>
2020-01-27 11:05:35 -0800

    title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get '/'
     assert_response :success
-    assert_select "title", "Home | Rails"
+    assert_select "title", "Rails App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Rails"
+    assert_select "title", "Help | Rails App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Rails"
+    assert_select "title", "About | Rails App"
   end
 
 end
