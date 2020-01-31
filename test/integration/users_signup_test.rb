381e067acc34df28e67f155babdbca8e0111ee96
Felix <felix.suen2000@gmail.com>
2020-01-30 22:15:45 -0800

    sign in flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,26 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get signup_path
+
+  test "invalid signup" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      before_count = User.count
+      post users_path, params: { user: { name: "", email: "user@invalid",
+        password: "food", password_confirmation: "skoom"} }
+    end
+    assert_template "users/new"
+  end
+
+  test "valid signup" do
+    get signup_path
+    assert_difference 'User.count' do
+      before_count = User.count
+      post users_path, params: { user: { name: "example",
+        email: "example@valid.com", password: "skooma",
+        password_confirmation: "skooma"} }
+    end
+    follow_redirect!
+    assert_template "users/show"
+  end
 end
